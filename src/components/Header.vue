15a1e25f0e25eeaf28ac8ba1fc34791cc4e733d3
Chandan <chandan7791@gmail.com>
2018-12-14 15:16:30 +0530

    Changes for Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,23 @@
 <template>
-    <ul class="nav nav-pills">
+<div>
+    <!-- <ul class="nav nav-pills">
         <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
-    </ul>
+    </ul> -->
+    <header class="main-header">
+      <div><a href="index.html" class="main-header__brand"> Avengers </a></div>
+      <nav class="main-nav">
+        <ul class="main-nav__items">
+          <li class="main-nav__item">
+            <a href="packages/index.html">Packages</a>
+          </li>
+          <li class="main-nav__item">
+            <a href="customers/index.html">Customers</a>
+          </li>
+          <li class="main-nav__item main-nav__item--cta">
+            <a href="star-hosting/index.html">Star Hosting</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+</div>
 </template>
